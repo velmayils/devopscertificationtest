c9d86dd6d4ba2ae2d1ceb0b790a1561e64dcc67c
velmayils <velmayilcse@gmail.com>
2021-06-12 13:56:25 +0530

    Update Main.java

--- src/main/java/com/edureka/test/Main.java
+++ src/main/java/com/edureka/test/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         WebDriver browser;
+        System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         browser = new ChromeDriver(options);
